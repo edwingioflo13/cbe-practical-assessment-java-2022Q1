2d3ec86fcd8fc5887f18970be9742d3f065fbe22
Edwin Flores <edwingioflo1396@gmail.com>
2022-04-01 09:45:16 -0500

    initialize project

---
+++ src/main/java/com/pichincha/msastore/MsaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.msastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaStoreApplication.class, args);
+    }
+
+}
